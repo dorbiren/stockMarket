c6e07db4e144a04e2b7770f355e79c1c0b18704a
dorbiren <dorbiren@gmail.com>
2018-03-30 11:37:17 +0300

    kaky virsion

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello pepole!");
+            Console.WriteLine("Hello world!");
+            //kakki
         }
     }
 }
