64d073a9f030f2d91e1842536f1416ef7c50f588
dorbiren <37333002+dorbiren@users.noreply.github.com>
2018-03-30 11:40:30 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
-            //kakki
+            //kakki #2
         }
     }
 }
